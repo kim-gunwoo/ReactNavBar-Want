87e5fdfa79a35e597821ae7496018dba494b88b3
git-k <kgunu93@gmail.com>
2021-07-18 18:28:50 +0900

    초기값 수정

--- src/utils/useResponsive.js
+++ src/utils/useResponsive.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useCallback } from "react";
 export const MOBILE_SCREEN = 825;
 
 export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= MOBILE_SCREEN);
 
   const handleWindowResize = useCallback(
     (event) => {
@@ -26,26 +26,4 @@ export const useIsMobile = () => {
   }, [handleWindowResize]);
 
   return isMobile;
-};
-
-export const useOutsideClick = (el, initialState) => {
-  const [isActive, setIsActive] = useState(initialState);
-
-  useEffect(() => {
-    const onClick = (e) => {
-      if (el.current !== null && !el.current.contains(e.target)) {
-        setIsActive(!isActive);
-      }
-    };
-
-    if (isActive) {
-      window.addEventListener("click", onClick);
-    }
-
-    return () => {
-      window.removeEventListener("click", onClick);
-    };
-  }, [isActive, el]);
-
-  return [isActive, setIsActive];
 };
